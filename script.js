646c8bd5a6b2735443cdf07d60402569929e851a
kdaya <kdayathegamer@gmail.com>
2018-11-27 14:07:22 +1100

    deals with invalid entries

--- script.js
+++ script.js
@@ -24,92 +24,15 @@ buttons.forEach((button) => {
 
 function calculate(values) {
   let result = values.join("");
-  return eval(result);
+  // return evil(result);
+  try {
+    return doMath(result);
+  }
+  catch(err) {
+    return "Invalid input!";
+  }
 }
 
-
-// Old code
-// let tempa = 0
-// let a = [];
-// let operator = [];
-// let previousPress;
-// let index = 0;
-//
-// let buttons = document.querySelectorAll("button");
-// buttons.forEach((button) => {
-//   button.addEventListener('click', () => {
-//     pressed = button.textContent;
-//     a[index] = 0;
-//     if(pressed == "=") {
-//       a[index] = tempa;
-//       operate(a, operator)
-//     } else if (pressed == "CL") {
-//       operator = null;
-//       a = null;
-//     } else if (pressed == "+" || pressed == "-" || pressed == "*" || pressed == "/") {
-//       if (!(previousPress == "+" || previousPress == "-" || previousPress == "*" || previousPress == "/")) {
-//           operator[index] = pressed;
-//           previousPress = pressed;
-//           a[index] = tempa;
-//           tempa = 0;
-//           index++;
-//       } else {
-//         console.log("invalid");
-//       }
-//     } else {
-//       tempa += pressed;
-//       previouspress = pressed;
-//     }
-//   })
-// });
-//
-//
-// function operate(num, operator) {
-//   let answer = 0;
-//   for(i=0; i < operator.length; i++) {
-//     if (operator[i] == "+") {
-//       answer += add(num);
-//     } else if (operator[i] == "-") {
-//       answer += subtract(num);
-//     } else if (operator[i] == "*") {
-//       answer += multiply(num);
-//     } else if (operator[i] == "/") {
-//       answer += divide(num);
-//     }
-//   }
-//   console.log(answer);
-// }
-//
-// function add(num) {
-// 	let add = 0;
-// 	for(let i = 0; i < 2; i++) {
-//     console.log(Number(num[i]));
-// 		add += Number(num[i]);
-// 	}
-// 	return add;
-// }
-//
-// function subtract (num) {
-// 	let subtract = num[0];
-// 	for (let i = 1; i < num.length; i++) {
-// 		subtract -= Number(num[i]);
-// 	}
-// 	return subtract;
-// }
-//
-// function multiply (num) {
-// 	let multiply = 1;
-// 	for (let i = 0; i < num.length; i++) {
-// 		multiply *= num[i];
-// 	}
-// 	return multiply;
-// }
-//
-// function divide(num) {
-//   let divide = num[0];
-//   for(let i = 1; i < num.length; i++) {
-//     console.log(Number(num[i]));
-//     divide /= Number(num[i]);
-//   }
-//   return divide;
-// }
+function doMath(fn) {
+  return new Function('return ' + fn)();
+}
