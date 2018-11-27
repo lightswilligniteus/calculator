87c0e0fa3084d93d1789ce68627309a64060aaba
kdaya <kdayathegamer@gmail.com>
2018-11-27 14:53:44 +1100

    place all operations in functions

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-// Array appended to with each value, seperated by an operator
-// The calculator checks for syntax (e.g. ++ doesn't wrok)
-// = is the 'go bnutton'
-// . ( ) + - / * = and 0-9
-// Needs to deal with negatives (possible plus minus button?)
-
-let tempValue = [];
-
-let buttons = document.querySelectorAll("button");
-let textBox = document.querySelector("input");
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    if (button.textContent == "=") {
-        textBox.value = calculate(tempValue);
-    } else if (button.textContent == "CL") {
-       tempValue = [];
-       textBox.value = tempValue;
-    } else {
-      tempValue.push(button.textContent);
-      textBox.value = tempValue.join("");
-    }
+function operate() {
+  let tempValue = [];
+  let buttons = document.querySelectorAll("button");
+  let textBox = document.querySelector("input");
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      if (button.textContent == "=") {
+          textBox.value = calculate(tempValue);
+      } else if (button.textContent == "CL") {
+         tempValue = [];
+         textBox.value = tempValue;
+      } else {
+        tempValue.push(button.textContent);
+        textBox.value = tempValue.join("");
+      }
+    });
   });
-});
+}
 
 function calculate(values) {
   let result = values.join("");
-  // return evil(result);
   try {
-    return doMath(result).toFixed(2);
+    return doMath(result);
   }
   catch(err) {
     return "Invalid input!";
@@ -36,3 +30,5 @@ function calculate(values) {
 function doMath(fn) {
   return new Function('return ' + fn)();
 }
+
+operate();
